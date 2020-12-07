be15fa008e7e697eca0bcbec2635456e92c1c0d0
yassunsun <ww16471z@icloud.com>
2020-12-07 11:29:04 +0900

    Revert "Merge pull request #8 from yassunsun/second_training"
    This reverts commit cb58ec880cb6c6f3ae2417f519be34d5243e2d80, reversing
    changes made to 575d87fae9291125d242c0a45de46b4860195e66.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -15,7 +15,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:plan).permit(:date, :plan)
+    params.require(:calendars).permit(:date, :plan)
   end
 
   def get_week
@@ -23,7 +23,6 @@ class CalendarsController < ApplicationController
 
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
-
     # 例)今日が2月1日の場合・・・ Date.today.day: '1日'
 
     @week_days = []
@@ -35,16 +34,7 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-
-      wday_num = Date.today.wday + x
-      # wdayメソッドを用いて取得した数値
-      # 「+ x」がないと日にちが動かないので、同じ数字が入り続ける。
-      if wday_num >= 7 #「wday_numが7以上の場合」という条件式
-        wday_num = wday_num -7 # 曜日は0〜6なので、7になると曜日を取り出せない。
-      end
-
-      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: wdays[wday_num] }
-      # :wday => wdays[wday_num]   wdays(曜日)はwday_num(何番目の曜日)なのかをキー(:wday)に渡す。
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
       @week_days.push(days)
     end
 
